c241b47a63a96649ce3cc89ce2aa189412ab93ae
dasyx <seanpnn00@gmail.com>
2025-01-13 18:12:56 +0100

    component navbar delete user base restored

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -36,53 +36,34 @@
         :class="active ? 'is-active' : ''"
       >
         <div class="navbar-start">
-          <a
-            v-for="presentation in presentations"
-            :key="presentation.name"
-            class="navbar-item hoverEffect"
-            :href="presentation.url"
-            presentation
-          >
-            {{ presentation.name }}
-          </a>
+          <!-- <a class="navbar-item hoverEffect" href="/"> Accueil </a> -->
+          <router-link to="/certification" class="navbar-item hoverEffect">
+            Certifications
+          </router-link>
+
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link" href="#navbarHeader">Plus</a>
+            <a class="navbar-link" href="#navbarHeader"> Plus </a>
 
             <div class="navbar-dropdown">
               <router-link
                 v-if="userIsLogged"
                 to="/formation"
                 class="navbar-item hoverEffect"
-                >Programme de formation</router-link
               >
-              <router-link to="/certification" class="navbar-item hoverEffect"
-                >Certifications</router-link
-              >
-              <router-link to="/qualite" class="navbar-item hoverEffect"
-                >Qualité</router-link
-              >
-
-              <!-- Liens de téléchargements conditionnels pour "cartographie tarifs" et "tarifs" -->
-              <a
-                v-if="userIsLogged"
+                Programme de formation
+              </router-link>
+              <router-link to="/qualite" class="navbar-item hoverEffect">
+                Qualité
+              </router-link>
+              <!--  <a
                 class="navbar-item hoverEffect"
-                href="tarifs/cartographie_tarifs.pdf"
-                download
-              >
-                Cartographie tarifs
-              </a>
+                :href="cataLink"
+                aria-label="Télécharger catalogue"
+                download="fichier"
+                >{{ cata }} En construction...
+              </a> -->
               <a
-                v-if="userIsLogged"
-                class="navbar-item hoverEffect"
-                href="tarifs/tarifs_REG_2024.pdf"
-                download
-              >
-                Tarifs
-              </a>
-
-              <!-- Autres liens de téléchargement -->
-              <a
-                v-for="download in filteredDownloads"
+                v-for="download in downloads"
                 :key="download.name"
                 class="navbar-item hoverEffect"
                 :href="download.url"
@@ -90,19 +71,29 @@
               >
                 {{ download.name }}
               </a>
-
               <a
                 class="navbar-item hoverEffect"
                 href="https://www.ouformer.com/organisme-de-formation/ML-Industrie"
                 target="_blank"
-                >Calendrier Inter</a
+              >
+                Calendrier Inter</a
               >
               <hr class="navbar-divider" />
-              <a class="navbar-item" href="#footer">En savoir plus</a>
+              <a class="navbar-item" href="#footer"> En savoir plus</a>
             </div>
           </div>
         </div>
-
+        <!-- Affichage du titre principal de la page-->
+        <div class="navbar-item navbar-item_title hoverEffect">
+          <p v-if="windowDimensions.width >= 767">
+            {{ societe }}
+          </p>
+          <p v-if="windowDimensions.width >= 767">
+            {{ titre }}
+          </p>
+        </div>
+        <!-- Fin -->
+        <!-- Affichage des boutons de connexions -->
         <div class="navbar-end">
           <div class="navbar-item">
             <div v-if="!userIsLogged" class="buttons">
@@ -127,7 +118,8 @@
             </div>
           </div>
         </div>
-
+        <!-- Fin -->
+        <!-- Modal de confirmation -->
         <div v-if="showModal" class="modal">
           <div class="modal-background"></div>
           <div class="modal-content">
@@ -141,63 +133,76 @@
             <button class="button" @click="cancelDelete">Annuler</button>
           </div>
         </div>
+        <!-- Fin -->
       </div>
     </nav>
   </header>
 </template>
 
-<script>
+<script setup>
 import { ref, onMounted, onUnmounted, computed, watchEffect } from "vue";
 import { useStore } from "vuex";
 import axios from "axios";
 import store from "../modules/store.json";
 import { useStorage } from "@vueuse/core";
 
-// Variables et références
 const userIsLogged = ref(false);
 const user = ref("");
+const isActive = ref("");
+const societe = ref("ML INDUSTRIE");
+const titre = ref("Des formations fiables et efficaces");
 const active = ref(false);
-const presentations = ref([{ name: "Présentation", url: "/ml_plaquette.pdf" }]);
-const store = useStore();
+const showNavbar = ref(true);
+const windowDimensions = ref({ width: 0, height: 0 });
+const downloads = ref([
+  { name: "cartographie tarifs", url: "tarifs/cartographie_tarifs.pdf" },
+  { name: "tarifs", url: "tarifs/tarifs_REG_2024.pdf" },
+  { name: "cnil", url: "/cnil.pdf" },
+]);
+
+const store1 = useStore();
+const cataLink = computed(() => store1.state.cataLink);
+
 const userToken = useStorage("user-token", null, sessionStorage);
 const userId = useStorage("user-id", null, sessionStorage);
+
 const showModal = ref(false);
 
-// Vérifier le statut de l'utilisateur
+// Récupération du token et vérification du statut de connexion de l'utilisateur
 const checkUserStatus = () => {
-  const token = sessionStorage.getItem("user-token");
+  const token = sessionStorage.getItem("user-token"); // Remplacer 'userToken' par la clé réelle utilisée
   if (token) {
+    // Appel API ou autre méthode pour valider le token et récupérer les infos de l'utilisateur
     axios
-      .get(`${store.api_localhost}/user/${userId.value}`, {
+      .get(`${store.api_host}/user/${userId.value}`, {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
-        user.value = response.data;
+        console.log(response.data);
+        user.value = response.data; // Supposer que le serveur renvoie les données de l'utilisateur
       })
       .catch(() => {
-        sessionStorage.removeItem("user-token");
+        // Gérer l'erreur, par exemple en effaçant le token invalide
+        // localStorage.removeItem("user-token");
       });
   }
 };
-
-// Afficher la confirmation de suppression
+// Modifier la fonction deleteAccount pour afficher le modal
 const deleteAccount = () => {
-  console.log("deleteAccount appelé"); // Vérification du clic
   showModal.value = true;
-  console.log("showModal:", showModal.value); // Vérification
 };
 
-// Annuler la suppression
+// Ajouter une fonction pour annuler la suppression
 const cancelDelete = () => {
   showModal.value = false;
 };
 
-// Confirmer la suppression
+// Ajouter une nouvelle fonction pour confirmer la suppression
 const confirmDelete = () => {
   const token = sessionStorage.getItem("user-token");
   if (token) {
     axios
-      .delete(`${store.api_localhost}/user/${userId.value}`, {
+      .delete(`${store.api_host}/user/${userId.value}`, {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
@@ -211,28 +216,49 @@ const confirmDelete = () => {
   showModal.value = false;
 };
 
-// Déconnexion de l'utilisateur
 const logoutUser = () => {
-  sessionStorage.removeItem("user-token");
-  userIsLogged.value = false;
+  sessionStorage.removeItem("user-token"); // Clear the token
+  userIsLogged.value = false; // Update the logged-in state
+  // Implement any additional logout logic or redirection if necessary
   location.reload();
 };
 
-// Gestion de la taille de l'écran
 const handleResize = () => {
-  active.value = false;
+  if (window) {
+    // Make sure window is defined
+    windowDimensions.value.width = window.innerWidth;
+    windowDimensions.value.height = window.innerHeight;
+  }
 };
 
-// Afficher ou cacher le menu mobile
+const swingOnLoad = () => {
+  const element = document.getElementById("animateLogo");
+  element.classList.add("swing");
+};
+
+const respNav = () => {
+  if (windowDimensions.value.width <= 767) {
+    isActive.value = true;
+    document.getElementById("lowResH1").innerHTML =
+      societe.value + ", " + titre.value;
+  }
+  if (windowDimensions.value.width >= 768) {
+    isActive.value = false;
+    document.querySelector(".displayHr").remove("displayHr");
+  }
+};
+
 const showMobileMenu = () => {
+  console.log("is-active");
   active.value = !active.value;
 };
 
-// Lifecycle hooks
 onMounted(() => {
-  console.log("Navbar chargé"); // Vérification
-  checkUserStatus();
+  checkUserStatus(); // Vérifier le statut de connexion lors du montage du composant
   window.addEventListener("resize", handleResize);
+  handleResize();
+  respNav();
+  swingOnLoad();
 });
 
 onUnmounted(() => {
@@ -240,7 +266,7 @@ onUnmounted(() => {
 });
 
 watchEffect(() => {
-  userIsLogged.value = !!userToken.value;
+  userIsLogged.value = !!userToken.value; // This will be true if userToken is not null or empty
 });
 </script>
 
